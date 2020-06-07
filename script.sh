515e3dc0fccf7c4d04e94579aeef0846fdf4c2ef
Owen Conti <owen@ohseemedia.com>
2020-06-06 19:36:37 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,45 +1,35 @@
-# uninstall npm dependencies
+# tailwind preset with auth
+composer require laravel-frontend-presets/tailwindcss --dev
+php artisan ui tailwindcss --auth
 
+# uninstall npm dependencies
 npm uninstall sass sass-loader
 
 # install npm dependencies
-
 npm install tailwindcss @ohseesoftware/tailwind-config @tailwindcss/ui @inertiajs/inertia-vue @inertiajs/inertia vue axios lodash vue-meta ziggy
 
-# install npm dev depdendencies
-
+# install npm dev dependencies
 npm install --save-dev @babel/preset-env @babel/plugin-syntax-dynamic-import @babel/parser babel-eslint eslint eslint-config-prettier eslint-import-resolver-alias eslint-plugin-import eslint-plugin-prettier eslint-plugin-vue postcss-import postcss-nested prettier resolve-url-loader
 
 # install composer depdendencies
-
 composer require tightenco/ziggy squizlabs/php_codesniffer ohseesoftware/laravel-schema-list laravel/ui inertiajs/inertia-laravel
 
-# install tailwind preset
-
-composer require laravel-frontend-presets/tailwindcss --dev
-
 # tailwind.config.js
-
 curl https://raw.githubusercontent.com/ohseesoftware/laravel-boilerplate-script/master/tailwind.config.js --output tailwind.config.js
 
 # phpcs.xml
-
 curl https://github.com/ohseesoftware/phpcs-config/blob/master/phpcs.xml --output phpcs.xml
 
 # phpunit.xml
-
 curl https://raw.githubusercontent.com/ohseesoftware/prettier-config/master/phpunit.xml --output phpunit.xml
 
 # PrettierPHPFixer.php
-
 curl https://raw.githubusercontent.com/ohseesoftware/prettier-php-fixer/master/PrettierPHPFixer.php --output PrettierPHPFixer.php
 
 # .prettierrc
-
 curl https://raw.githubusercontent.com/ohseesoftware/prettier-config/master/.prettierrc --output .prettierrc
 
 # setup frontend resources
-
 rm -rf resources/js/*
 rm -rf resources/css/*
 
@@ -50,9 +40,4 @@ curl https://raw.githubusercontent.com/ohseesoftware/laravel-boilerplate-script/
 curl https://raw.githubusercontent.com/ohseesoftware/laravel-boilerplate-script/master/ZiggyMixin.js --output resources/js/mixins/ZiggyMixin.js
 
 # setup backend resources
-
 curl https://raw.githubusercontent.com/ohseesoftware/laravel-boilerplate-script/master/InertiaServiceProvider.php --output app/Providers/InertiaServiceProvider.php
-
-# run tailwind preset
-
-php artisan ui tailwindcss --auth
